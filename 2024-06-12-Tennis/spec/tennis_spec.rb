64622dc42223b39b87b562ffc66acd6d766284c4
Michael Milewski <saramic@gmail.com>
2024-06-12 19:10:48 +1000

    created test for scoring
    Co-authored-by: Rebekah Wu <80230837+rebekauw@users.noreply.github.com>

--- 2024-06-12-Tennis/spec/tennis_spec.rb
+++ 2024-06-12-Tennis/spec/tennis_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe "Tennis" do
     tennis = Tennis.new
     expect(tennis.score).to eq "love all"
   end
+  it "if server wins a point, the score is 15 love" do
+    tennis = Tennis.new
+    pending "someone creates point server method"
+    tennis.point_server
+    expect(tennis.score).to eq "15 love"
+  end
 end
+
