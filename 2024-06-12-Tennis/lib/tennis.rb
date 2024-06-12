770b1e167ad7f264713953b45a1e9e077acc3989
Michael Milewski <saramic@gmail.com>
2024-06-12 19:42:07 +1000

    Impleted server can win two points logic
    Co-authored-by: Alexander Resnelis <129151965+aresnelis@users.noreply.github.com>

--- 2024-06-12-Tennis/lib/tennis.rb
+++ 2024-06-12-Tennis/lib/tennis.rb
@@ -1,15 +1,20 @@
 class Tennis
+  # SCORES = {
+  #   0: "love",
+  #   1: "15",
+  #   2: "30"
+  # }
   def initialize
-    @something = false
+    @score = 0
   end
 
   def score
-    return "15 love" if @something
-
-    "love all"
+    return "love all" if @score == 0
+    return "15 love" if @score == 1
+    return "30 love" if @score == 2
   end
 
   def point_server
-    @something = true
+    @score += 1
   end
 end
--- 2024-06-12-Tennis/spec/tennis_spec.rb
+++ 2024-06-12-Tennis/spec/tennis_spec.rb
@@ -3,13 +3,20 @@ require "tennis"
 RSpec.describe "Tennis" do
   it "starts a game with love all" do
     tennis = Tennis.new
+    0.times { tennis.point_server }
     expect(tennis.score).to eq "love all"
   end
+
   it "if server wins a point, the score is 15 love" do
     tennis = Tennis.new
-    "someone creates point server method"
-    tennis.point_server
+    1.times { tennis.point_server }
     expect(tennis.score).to eq "15 love"
+  end
+
+  it "if server wins 2 points in a row, the score is 30 love" do
+    tennis = Tennis.new
+    2.times { tennis.point_server }
+    expect(tennis.score).to eq "30 love"
   end
 end
 
