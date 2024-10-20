b3ade634cd6b8f3425edc663842f9d73fc730285
Michael Milewski <saramic@gmail.com>
2024-10-21 09:25:51 +1100

    Implement rotate left and right

--- 2024-10-21-mars-rover-py/rover.py
+++ 2024-10-21-mars-rover-py/rover.py
@@ -1,2 +1,32 @@
+DIRECTIONS = ["N", "E", "S", "W"]
+DIRECTION_OFFSET = {
+        "r": 1,
+        "l": -1
+        }
+
+
 def rover(position_commands):
-    return {"x": 0, "y": 0, "direction": "N"}
+    for command in position_commands["commands"]:
+        apply_command(position_commands, command)
+    return {
+            "x": position_commands["x"],
+            "y": position_commands["y"],
+            "direction": position_commands["direction"]}
+
+
+def apply_command(position_commands, command):
+    if command in ["l", "r"]:
+        apply_turn(position_commands, command)
+
+
+def apply_turn(position_commands, command):
+    position_commands.update({
+        "direction": new_direction(position_commands["direction"], command)
+    })
+    return position_commands
+
+
+def new_direction(direction, command):
+    current_index = DIRECTIONS.index(direction)
+    new_index = (current_index + DIRECTION_OFFSET[command]) % len(DIRECTIONS)
+    return DIRECTIONS[new_index]
--- 2024-10-21-mars-rover-py/rover_test.py
+++ 2024-10-21-mars-rover-py/rover_test.py
@@ -5,3 +5,41 @@ def test_stationary_rover():
     assert rover({
         "x": 0, "y": 0, "direction": "N", "commands": []
         }) == {"x": 0, "y": 0, "direction": "N"}
+
+
+def test_rotates_right():
+    location = rover({
+        "x": 0, "y": 0, "direction": "N", "commands": []
+        })
+    assert location == {"x": 0, "y": 0, "direction": "N"}
+    location.update({"commands": ["r"]})
+    location = rover(location)
+    assert location == {"x": 0, "y": 0, "direction": "E"}
+    location.update({"commands": ["r"]})
+    location = rover(location)
+    assert location == {"x": 0, "y": 0, "direction": "S"}
+    location.update({"commands": ["r"]})
+    location = rover(location)
+    assert location == {"x": 0, "y": 0, "direction": "W"}
+    location.update({"commands": ["r"]})
+    location = rover(location)
+    assert location == {"x": 0, "y": 0, "direction": "N"}
+
+
+def test_rotates_left():
+    location = rover({
+        "x": 0, "y": 0, "direction": "N", "commands": []
+        })
+    assert location == {"x": 0, "y": 0, "direction": "N"}
+    location.update({"commands": ["l"]})
+    location = rover(location)
+    assert location == {"x": 0, "y": 0, "direction": "W"}
+    location.update({"commands": ["l"]})
+    location = rover(location)
+    assert location == {"x": 0, "y": 0, "direction": "S"}
+    location.update({"commands": ["l"]})
+    location = rover(location)
+    assert location == {"x": 0, "y": 0, "direction": "E"}
+    location.update({"commands": ["l"]})
+    location = rover(location)
+    assert location == {"x": 0, "y": 0, "direction": "N"}
