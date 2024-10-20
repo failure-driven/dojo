181946b2504b08cb4fd2e89172760edcd7bf0446
Michael Milewski <saramic@gmail.com>
2024-10-20 15:04:32 +1100

    A single function approach to tennis in python🐍

--- 2024-10-20-tennis-py/tennis.py
+++ 2024-10-20-tennis-py/tennis.py
@@ -1,2 +1,59 @@
-def tennis():
-    return "love all"
+SERVER = "server"
+RECEIVER = "receiver"
+ADVANTAGE_DIFF = 1
+NORMAL_SCORE = {
+        0: "love",
+        1: "15",
+        2: "30",
+        3: "40"
+        }
+
+
+def tennis(points=[]):
+    score = setup_score(points)
+    return advantage_score(score) or \
+        winning_score(score) or \
+        normal_score(score)
+
+
+def setup_score(points):
+    score = {SERVER: 0, RECEIVER: 0}
+    for point in points:
+        score[point] += 1
+    return score
+
+
+def leading(score):
+    return max([p for p in score.keys()], key=lambda x: score[x])
+
+
+def advantage_score(score):
+    if score_abs_eq_one(score) and above_normal_score(score):
+        return f"advantage {leading(score)}"
+
+
+def winning_score(score):
+    if score_abs_gte_two(score) and above_normal_score(score):
+        return f"game {leading(score)}"
+
+
+def normal_score(score):
+    scores = [
+            NORMAL_SCORE[score[SERVER] or 0],
+            NORMAL_SCORE[score[RECEIVER] or 0]
+            ]
+    if max(score.values()) == min(score.values()):
+        scores = [scores[0], "all"]
+    return " ".join(scores)
+
+
+def above_normal_score(score):
+    return max(score.values()) > max(NORMAL_SCORE.keys())
+
+
+def score_abs_gte_two(score):
+    return (max(score.values()) - min(score.values())) > ADVANTAGE_DIFF
+
+
+def score_abs_eq_one(score):
+    return (max(score.values()) - min(score.values())) == ADVANTAGE_DIFF
--- 2024-10-20-tennis-py/tennis_test.py
+++ 2024-10-20-tennis-py/tennis_test.py
@@ -3,3 +3,56 @@ from tennis import tennis
 
 def test_start_game():
     assert tennis() == 'love all'
+
+
+def test_server_first_point():
+    assert tennis(["server"]) == '15 love'
+
+
+def test_server_wins_game():
+    assert tennis(
+            ["server", "server", "server", "server"]
+            ) == 'game server'
+
+
+def test_server_advantage():
+    assert tennis(
+            ["server", "server", "server", "receiver", "receiver", "receiver",
+             "server"]
+            ) == 'advantage server'
+
+
+def test_server_30_love():
+    assert tennis(
+            ["server", "server"]
+            ) == '30 love'
+
+
+def test_server_40_love():
+    assert tennis(
+            ["server", "server", "server"]
+            ) == '40 love'
+
+
+def test_receiver_love_15():
+    assert tennis(
+            ["receiver"]
+            ) == 'love 15'
+
+
+def test_receiver_love_30():
+    assert tennis(
+            ["receiver", "receiver"]
+            ) == 'love 30'
+
+
+def test_receiver_love_40():
+    assert tennis(
+            ["receiver", "receiver", "receiver"]
+            ) == 'love 40'
+
+
+def test_receiver_30_all():
+    assert tennis(
+            ["receiver", "receiver", "server", "server"]
+            ) == '30 all'
