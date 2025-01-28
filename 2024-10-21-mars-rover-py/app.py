b9fc6b7946e292bd632a8cbe9476a6a84239992b
Michael Milewski <saramic@gmail.com>
2025-01-28 20:59:57 +1100

    Get around flask_cors not working

--- 2024-10-21-mars-rover-py/app.py
+++ 2024-10-21-mars-rover-py/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from rover import rover
 
@@ -8,4 +8,6 @@ cors = CORS(app)
 
 @app.route('/', methods=['POST'])
 def application():
-    return rover(request.json)
+    response = jsonify(rover(request.json))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
