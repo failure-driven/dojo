64c7b0b066115026b8b2e72fde05deba1c1a02be
Michael Milewski <saramic@gmail.com>
2025-09-03 14:25:08 +1000

    Simplify the direction finding
    Co-authored-by: anthhoan <197538168+anthhoan@users.noreply.github.com>
    Co-authored-by: Ying <116133253+ying1488@users.noreply.github.com>
    Co-authored-by: camtimms <197161561+camtimms@users.noreply.github.com>
    Co-authored-by: pezza76 <197482796+pezza76@users.noreply.github.com>
    Co-authored-by: harmel3056 <203648511+harmel3056@users.noreply.github.com>
    Co-authored-by: XIA0LING CUI <182103719+xl-c111@users.noreply.github.com>
    Co-authored-by: Mat-26-dot <197480060+Mat-26-dot@users.noreply.github.com>

--- 2025-09-03-mars-rover-py/rover.py
+++ 2025-09-03-mars-rover-py/rover.py
@@ -9,16 +9,8 @@ def rover(position_commands):
     x = position_commands["x"]
     y = position_commands["y"]
     direction = position_commands["direction"]
-    if position_commands["commands"] == ["r"] and direction == DIRECTIONS[0]:
-        direction = DIRECTIONS[1]
-
-    if position_commands["commands"] == ["r"] and direction == DIRECTIONS[2]:
-        direction = DIRECTIONS[3]
-
-    if position_commands["commands"] == ["r", "r"] and direction == DIRECTIONS[0]:
-        direction = DIRECTIONS[2]
-    index = 0
-    if position_commands["commands"] == ["r", "r", "r"] and direction == DIRECTIONS[index]:
-        index += 3 # because 3 "r"'s
-        direction = DIRECTIONS[index]
+    index = DIRECTIONS.index(direction)
+    for command in position_commands['commands']:
+        index += 1
+    direction = DIRECTIONS[index]
     return {"x": x, "y": y, "direction": direction}
