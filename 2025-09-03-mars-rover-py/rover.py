7cb30ac638c407caa15ee495eedbb7b3eb26d45a
Michael Milewski <saramic@gmail.com>
2025-09-03 14:03:12 +1000

    Added directions array
    Co-authored-by: anthhoan <197538168+anthhoan@users.noreply.github.com>
    Co-authored-by: Ying <116133253+ying1488@users.noreply.github.com>
    Co-authored-by: camtimms <197161561+camtimms@users.noreply.github.com>
    Co-authored-by: pezza76 <197482796+pezza76@users.noreply.github.com>

--- 2025-09-03-mars-rover-py/rover.py
+++ 2025-09-03-mars-rover-py/rover.py
@@ -1,9 +1,20 @@
+
+DIRECTIONS = [
+    "N",
+    "E",
+    "S",
+    "W",
+]
 def rover(position_commands):
     x = position_commands["x"]
     y = position_commands["y"]
     direction = position_commands["direction"]
-    if position_commands["commands"] == ["r"]:
-        direction = "E"
-    if position_commands["commands"] == ["r", "r"]:
-        direction = "S"
+    if position_commands["commands"] == ["r"] and direction == DIRECTIONS[0]:
+        direction = DIRECTIONS[1]
+
+    if position_commands["commands"] == ["r"] and direction == DIRECTIONS[2]:
+        direction = DIRECTIONS[3]
+
+    if position_commands["commands"] == ["r", "r"] and direction == DIRECTIONS[0]:
+        direction = DIRECTIONS[2]
     return {"x": x, "y": y, "direction": direction}
