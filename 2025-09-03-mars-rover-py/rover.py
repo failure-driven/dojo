d3ac8dae558b017f4d46c30732bf49ac4b486d0f
Michael Milewski <saramic@gmail.com>
2025-09-03 13:49:13 +1000

    rotate right twice test

--- 2025-09-03-mars-rover-py/rover.py
+++ 2025-09-03-mars-rover-py/rover.py
@@ -1,5 +1,9 @@
 def rover(position_commands):
     x = position_commands["x"]
+    y = position_commands["y"]
+    direction = position_commands["direction"]
     if position_commands["commands"] == ["r"]:
-        return {"x": x, "y": 0, "direction": "E"}
-    return {"x": x, "y": 0, "direction": "N"}
+        direction = "E"
+    if position_commands["commands"] == ["r", "r"]:
+        direction = "S"
+    return {"x": x, "y": y, "direction": direction}
--- 2025-09-03-mars-rover-py/rover_test.py
+++ 2025-09-03-mars-rover-py/rover_test.py
@@ -6,7 +6,18 @@ def test_stationary_rover():
         "x": 0, "y": 0, "direction": "N", "commands": []
         }) == {"x": 0, "y": 0, "direction": "N"}
 
+
 def test_stationary_rover_360():
     assert rover({
         "x": 0, "y": 0, "direction": "N", "commands": ["r"]
         }) == {"x": 0, "y": 0, "direction": "E"}
+
+    assert rover({
+        "x": 0, "y": 0, "direction": "N", "commands": ["r", "r"]
+        }) == {"x": 0, "y": 0, "direction": "S"}
+
+
+def test_right_rotation():
+    assert rover({
+        "x": 0, "y": 0, "direction": "S", "commands": ["r"]
+        }) == {"x": 0, "y": 0, "direction": "W"}
