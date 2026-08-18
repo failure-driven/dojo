1785a590fa37036c28e07058f6a52353149a6ac2
Michael Milewski <saramic@gmail.com>
2026-08-18 13:54:02 +1000

    Process mini commands
    Co-authored-by: 14808-Lucas <293258564+14808-Lucas@users.noreply.github.com>
    Co-authored-by: Ethan-of-Aussie <258693946+Ethan-of-Aussie@users.noreply.github.com>
    Co-authored-by: Munya Murare <195309209+DziRare@users.noreply.github.com>
    Co-authored-by: jeff-holberton <293737243+jeff-holberton@users.noreply.github.com>
    Co-authored-by: Aladar Apponyi <60247835+papadar@users.noreply.github.com>
    Co-authored-by: Yasmin So <75427310+MissYasminSo@users.noreply.github.com>

--- 2026-08-18-mars-rover-py/src/rover/rover.py
+++ 2026-08-18-mars-rover-py/src/rover/rover.py
@@ -1,11 +1,12 @@
 TURN_LIST = ["N", "E", "S", "W"]
 
+
 def rover(position_commands):
 
-    if len(position_commands["commands"]) > 0:
-        offset = 1 if position_commands["commands"] == ["R"] else -1
-        current_direction_index = TURN_LIST.index(position_commands["direction"])
-        new_index = (current_direction_index + offset) % len(TURN_LIST)
+    for command in position_commands["commands"]:
+        offset = 1 if command == "R" else -1
+        curr_direction_index = TURN_LIST.index(position_commands["direction"])
+        new_index = (curr_direction_index + offset) % len(TURN_LIST)
         position_commands["direction"] = TURN_LIST[new_index]
 
     return {"x": 0, "y": 0, "direction": position_commands["direction"]}
--- 2026-08-18-mars-rover-py/src/rover/rover_test.py
+++ 2026-08-18-mars-rover-py/src/rover/rover_test.py
@@ -12,22 +12,32 @@ def test_East_rover():
         "x": 0, "y": 0, "direction": "N", "commands": ["R"]
     }) == {"x": 0, "y": 0, "direction": "E"}
 
+
 def test_right_again_rover():
     assert rover({
         "x": 0, "y": 0, "direction": "E", "commands": ["R"]
     }) == {"x": 0, "y": 0, "direction": "S"}
+
 
 def test_right_turn_from_west_rover():
     assert rover({
         "x": 0, "y": 0, "direction": "W", "commands": ["R"]
     }) == {"x": 0, "y": 0, "direction": "N"}
 
+
 def test_left_rover():
     assert rover({
         "x": 0, "y": 0, "direction": "N", "commands": ["L"]
     }) == {"x": 0, "y": 0, "direction": "W"}
 
+
 def test_right360_rover():
     assert rover({
         "x": 0, "y": 0, "direction": "N", "commands": ["R", "R", "R", "R"]
     }) == {"x": 0, "y": 0, "direction": "N"}
+
+
+def test_rover_forward():
+    assert rover({
+        "x": 0, "y": 0, "direction": "N", "commands": ["F"]
+    }) == {"x": 0, "y": 1, "direction": "N"}
