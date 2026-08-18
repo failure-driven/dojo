7c2e299b9053f4e85a05af2f9cca2bed6497e6ff
Michael Milewski <saramic@gmail.com>
2026-08-18 13:19:24 +1000

    Add tests for turning right again and failing test
    Co-authored-by: 14808-Lucas <293258564+14808-Lucas@users.noreply.github.com>
    Co-authored-by: Ethan-of-Aussie <258693946+Ethan-of-Aussie@users.noreply.github.com>
    Co-authored-by: Munya Murare <195309209+DziRare@users.noreply.github.com>

--- 2026-08-18-mars-rover-py/src/rover/rover.py
+++ 2026-08-18-mars-rover-py/src/rover/rover.py
@@ -1,5 +1,7 @@
 def rover(position_commands):
     if position_commands["commands"] == ["R"]:
-        position_commands["direction"] = "E"
+        right_turn_list = ["N", "E", "S", "W"]
+        current_direction_index = right_turn_list.index(position_commands["direction"])
+        position_commands["direction"] = right_turn_list[current_direction_index + 1]
 
     return {"x": 0, "y": 0, "direction": position_commands["direction"]}
--- 2026-08-18-mars-rover-py/src/rover/rover_test.py
+++ 2026-08-18-mars-rover-py/src/rover/rover_test.py
@@ -11,3 +11,13 @@ def test_East_rover():
     assert rover({
         "x": 0, "y": 0, "direction": "N", "commands": ["R"]
     }) == {"x": 0, "y": 0, "direction": "E"}
+
+def test_right_again_rover():
+    assert rover({
+        "x": 0, "y": 0, "direction": "E", "commands": ["R"]
+    }) == {"x": 0, "y": 0, "direction": "S"}
+
+def test_right_turn_from_west_rover():
+    assert rover({
+        "x": 0, "y": 0, "direction": "W", "commands": ["R"]
+    }) == {"x": 0, "y": 0, "direction": "N"}
